7d1f6e89abf87fc5926e120eb95451bb3624befe
jdains27 <dains.jordan2@gmail.com>
2021-02-03 08:19:10 -0600

    commit tree e216342588f24cb8273880a2fb2d9ed1488f8cfe

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,12 @@
-//your code here
+import { render } from 'enzyme';
+import React, { Component } from 'react';
+
+export default class Comment extends Component {
+render () {
+    return (
+        <div classname="comment">
+            {this.props.commentText}
+        </div>
+    );
+}
+}
